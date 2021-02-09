44b726280483a47772ed2b1280651344ab3abec4
ABRaihan <abraihan7244@gmail.com>
2021-02-09 21:36:42 +0600

    optimize the code

--- js/app.js
+++ js/app.js
@@ -1,76 +1,57 @@
 'use strict';
+/* -------------> All API URL <------------- */
 const mealByNameURL = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
 const mealByLetterURL = `https://www.themealdb.com/api/json/v1/1/search.php?f=`;
 const mealDetailsByIdURL = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=`;
+
 const form = document.querySelector('form');
 const search = document.querySelector('.searchButton');
 const foodRow = document.getElementById('foods');
 const ingredientsDetails = document.querySelector('#ingredientsDetails');
 const foodIngredients = document.querySelector('#foodIngredients');
 const foodItems = document.querySelector('#foodItems');
+const ingredientsClose = document.querySelector('.ingredientsClose');
+const notFoundCloseButton = document.querySelector('.notFoundClose');
 
 /* -------------> All EventListener <------------- */
 // Enter key press event
 form.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         e.preventDefault();
-        getFoodName();
+        showFoods();
     }
 });
 
 //Search Button click Event
-search.addEventListener('click', function (e) {
-    e.preventDefault();
-    getFoodName();
+search.addEventListener('click', function (event) {
+    event.preventDefault();
+    showFoods();
 });
 
 // this event is for showing food ingredients
-foodRow.addEventListener('click', function (e) {
-    if (!(this === e.target)) {
-        const mealID = e.target.parentElement.id;
-        fetch(`${mealDetailsByIdURL}${mealID}`)
-            .then((res) => res.json())
-            .then((data) => {
-                document.querySelector('.ingredientsImg').src =
-                    data.meals[0].strMealThumb;
-                document.querySelector('.ingredientsName').innerText =
-                    data.meals[0].strMeal;
-                for (let i = 1; i <= 20; i++) {
-                    let ingredients = data.meals[0][`strIngredient${i}`];
-                    console.log(typeof ingredients);
-                    if (ingredients !== '' && JSON.stringify(ingredients) !== 'null') {
-                        ingredientsDetails.innerHTML += `<li class="list-group-item"><i class="fas fa-check-square text-primary me-2"></i> ${
-                            data.meals[0][`strIngredient${i}`]
-                        }</li>`;
-                    }
-                }
-                foodIngredients.style.display = 'block';
-                foodIngredients.style.visibility = 'visible';
-                foodItems.style.display = 'none';
-            });
+foodRow.addEventListener('click', async function (event) {
+    const col = document.querySelectorAll('#foods .col-12');
+    if (!(this === event.target || [...col].find(element => event.target === element))) {
+        const mealID = event.target.parentElement.id;
+        const foodDetails = await getData(mealDetailsByIdURL, mealID);
+        showIngredients(foodDetails);
     }
 });
 
 // this event is close food ingredients section
-document.querySelector('.ingredientsClose').addEventListener('click', function (e) {
-    foodIngredients.style.display = 'none';
-    foodIngredients.style.visibility = 'hidden';
-    foodItems.style.display = 'block';
-    [...ingredientsDetails.children].forEach(child => child.remove());
-
-});
+ingredientsClose.addEventListener('click', function () {
+        foodIngredients.style.display = 'none';
+        foodIngredients.style.visibility = 'hidden';
+        foodItems.style.display = 'block';
+        [...ingredientsDetails.children].forEach((child) => child.remove());
+    });
 
 // this event close not found alert
-document.querySelector('.notFoundClose').addEventListener('click', function (e) {
-    document.querySelector('#notFoundAlert').style.transform = 'translate(-50%, -100%)';
-})
+notFoundCloseButton.addEventListener('click', function () {
+        document.querySelector('#notFoundAlert').style.transform =
+            'translate(-50%, -100%)';
+    });
 /* -------------> All Functions <------------- */
-// create HTML elements with attribute
-function createElements(tagName, attr, value) {
-    const tag = document.createElement(tagName);
-    tag.setAttribute(attr, value);
-    return tag;
-}
 
 // create card column
 function itemColumn() {
@@ -84,32 +65,64 @@ function itemColumn() {
     </div>`;
 }
 
-// get foods using API
-function getMeals(URL, foodName) {
-    fetch(`${URL}${foodName}`)
-        .then((res) => res.json())
-        .then((data) => {
-            if (JSON.stringify(data.meals) === 'null') {
-                document.querySelector('.alertMessage').innerText =`${foodName} Item Not Found`
-                document.querySelector('#notFoundAlert').style.transform = 'translate(-50%, 0%)';
-            } else showMeals(data);
-        });
+// get foods data using API
+async function getData(URL, target) {
+    const response = await fetch(`${URL}${target}`);
+    const data = await response.json();
+    return data;
 }
-function showMeals(data) {
-    for (let i = 0; i < data.meals.length; i++) {
+
+// this function will add food in food column
+function addMeals(data) {
+    data.meals.forEach((food, index)=> {
         itemColumn();
-        document.querySelectorAll('#foods img')[i].src = data.meals[i].strMealThumb;
-        document.querySelectorAll('h4')[i].innerText = data.meals[i].strMeal;
-        document.querySelectorAll('.card')[i].id = data.meals[i].idMeal;
-    }
+        document.querySelectorAll('#foods img')[index].src = food.strMealThumb;
+        document.querySelectorAll('h4')[index].innerText = food.strMeal;
+        document.querySelectorAll('.card')[index].id = food.idMeal;
+    })
 }
-// getting food name form input box and call getMeals() function
-function getFoodName() {
+
+// this function will show food not found alert
+function notFoundAlert(foodName) {
+    document.querySelector(
+        '.alertMessage'
+    ).innerText = `${foodName.value} Item Not Found`;
+    document.querySelector('#notFoundAlert').style.transform =
+        'translate(-50%, 0%)';
+}
+// getting food name form input box and call getData() function
+async function showFoods() {
     const foodName = document.querySelector('.foodName');
     [...foodRow.children].forEach((child) => child.remove());
     if (foodName.value) {
-        if (foodName.value.length > 1) getMeals(mealByNameURL, foodName.value);
-        else getMeals(mealByLetterURL, foodName.value);
+        if (foodName.value.length > 1) {
+            const mealsData = await getData(mealByNameURL, foodName.value);
+            if (JSON.stringify(mealsData.meals) === 'null')
+                notFoundAlert(foodName);
+            else addMeals(mealsData);
+        } else {
+            const mealsData = await getData(mealByLetterURL, foodName.value);
+            if (JSON.stringify(mealsData.meals) === 'null')
+                notFoundAlert(foodName);
+            else addMeals(mealsData);
+        }
     }
     foodName.value = '';
+}
+
+// this function will show food ingredients
+function showIngredients(data) {
+    document.querySelector('.ingredientsImg').src = data.meals[0].strMealThumb;
+    document.querySelector('.ingredientsName').innerText = data.meals[0].strMeal;
+    for (let i = 1; i <= 20; i++) {
+        let ingredients = data.meals[0][`strIngredient${i}`];
+        if (ingredients !== '' && JSON.stringify(ingredients) !== 'null') {
+            ingredientsDetails.innerHTML += `<li class="list-group-item"><i class="fas fa-check-square text-primary me-2"></i> ${
+                data.meals[0][`strIngredient${i}`]
+            }</li>`;
+        }
+    }
+    foodIngredients.style.display = 'block';
+    foodIngredients.style.visibility = 'visible';
+    foodItems.style.display = 'none';
 }
